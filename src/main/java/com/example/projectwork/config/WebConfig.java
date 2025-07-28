73b6439b7436b1efe043547f3421451a539933cb
chandana <gayamchandana@gmail.com>
2025-07-28 22:29:35 +0530

    Update WebConfig.java

--- src/main/java/com/example/projectwork/config/WebConfig.java
+++ src/main/java/com/example/projectwork/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5174",
-                                       "https://projexecelfrontend.netlify.app" )
+                                        "https://projexecelfrontend.netlify.app",       // Your deployed frontend
+                                      "https://projexcel-production-5f4e.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
