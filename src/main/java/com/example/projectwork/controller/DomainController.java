ec9907215057f6dad2d9e08166da771bf56cd3aa
chandana <gayamchandana@gmail.com>
2025-07-29 13:09:55 +0530

    Update DomainController.java

--- src/main/java/com/example/projectwork/controller/DomainController.java
+++ src/main/java/com/example/projectwork/controller/DomainController.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/domains")  // Matches frontend API call
-@CrossOrigin(origins = "http://localhost:5174", "https://projexecelfrontend.netlify.app",       // Your deployed frontend
-    "https://projexcel-production.up.railway.app")  // Vite dev server port
+@CrossOrigin(origins ={ "http://localhost:5174", "https://projexecelfrontend.netlify.app",       // Your deployed frontend
+    "https://projexcel-production.up.railway.app"}
+            )  // Vite dev server port
 public class DomainController {
 
     private final DomainRepository domainRepository;
