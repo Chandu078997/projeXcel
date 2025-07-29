2ec7fbf30db25b4461a67b2fa4f0625e2552994c
chandana <gayamchandana@gmail.com>
2025-07-29 09:06:14 +0530

    Update ProjectController.java

--- src/main/java/com/example/projectwork/controller/ProjectController.java
+++ src/main/java/com/example/projectwork/controller/ProjectController.java
@@ -88,7 +88,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/projects")
 @CrossOrigin(origins = "http://localhost:5174", "https://projexecelfrontend.netlify.app",       // Your deployed frontend
-    "https://projexcel-production-5f4e.up.railway.app")
+    "https://projexcel-production.up.railway.app"
+            )
 public class ProjectController {
 
     private final ProjectService projectService;
